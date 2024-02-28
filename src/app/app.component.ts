30635a39f0c433d9043b8d0969429bed8e02d2eb
CEIBA\pedro.pereira <ppereira@sura.com.co>
2024-02-28 10:56:14 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientServiceService  } from './client-service.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,35 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  data: Array<any> = [];
+
+  constructor(private clientServiceService: ClientServiceService) { }
+
+  ngOnInit(): void {
+    this.listAll();
+  }
+
+  listAll(): void {
+    
+      
+      this.clientServiceService.listAll().subscribe({
+        next: (data) => {
+          //capturar data
+          let datos: any;
+          datos = data;
+
+          //obtener datos
+          this.data = datos;
+
+        },
+        error: (e) => {
+          //Swal.fire('Error en el servidor', '', 'error');
+          console.error(e)
+          //this.spinner = false;
+        }
+      });
+      }
+
   title = 'dash-clientes-angular';
 }
