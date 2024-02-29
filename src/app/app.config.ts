4b08f3caa89c1b092788e62fbb27aada44c7f832
CEIBA\pedro.pereira <ppereira@sura.com.co>
2024-02-29 15:02:24 -0500

    ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient} from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -13,20 +11,3 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(HttpClientModule)
   ]
 };
-
-
-//import { provideAnimations } from '@angular/platform-browser/animations';
-//import { provideToastr } from 'ngx-toastr';
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes),
-//     importProvidersFrom(HttpClientModule),
-//     provideAnimations(),
-//     provideToastr({
-//       timeOut: 5000,
-//       positionClass: 'toast-top-center',
-//       preventDuplicates: true,
-//     }),
-//   ],
-// };
